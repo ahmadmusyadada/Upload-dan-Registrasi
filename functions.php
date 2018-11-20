b172b075aa82c3c19770312260751702a04e654f
ahmadmusyadada <ahmadmusyadadaminullah@gmail.com>
2018-11-20 18:07:05 +0700

    Registrasi

--- functions.php
+++ functions.php
@@ -130,4 +130,41 @@ function upload(){
     move_uploaded_file($tmpfile, 'image/'.$namafilebaru);
     return $namafilebaru;
 }
+
+function registrasi($data){
+    global $conn;
+
+    $username=strtolower(stripcslashes($data['username']));
+
+    $password=mysqli_real_escape_string($conn, $data['password']);
+    $password2=mysqli_real_escape_string($conn, $data['password2']);
+
+    $result=mysqli_query($conn, "SELECT username FROM users WHERE username='$username'");
+
+    if(mysqli_fetch_assoc($result)){
+        echo "
+            <script>
+                alert('username sudah ada');
+            </script>
+        ";
+        return false;
+    }
+
+    if($password!==$password2){
+        echo "
+            <script>
+                alert('password anda tidak sama');
+            </script>
+        ";
+        return false;
+    }
+
+    $password=md5($password);
+    // $password=password_hash($password, PASSWORD_DEFAULT);
+    var_dump($password);
+
+    mysqli_query($conn, "INSERT INTO users VALUES ('', '$username', '$password')");
+    
+    return mysqli_affected_rows($conn);
+}
 ?>
